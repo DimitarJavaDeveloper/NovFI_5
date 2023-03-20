e55142d0b545640e99d250ddc36c14c1c24fb42d
unknown <dimitarbelcovski29@gmail.com>
2023-03-20 12:16:27 +0100

    Add changes

--- src/Vezba5.java
+++ src/Vezba5.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Vezba5 {
+	public static void main(String[] args) {
+		int a,b,c,d,k;
+		Scanner tastatura = new Scanner(System.in);
+		System.out.println("Vnesi ja vrednosta na k")
+		
+		
+		System.out.println("Vnesi ja vrednosta na k")
+		
+	}
 
 }
