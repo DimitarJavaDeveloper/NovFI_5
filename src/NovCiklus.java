bbc044925010a04079c797fb4d266e346600600e
unknown <dimitarbelcovski29@gmail.com>
2023-03-20 11:38:40 +0100

    Add a comment

---
+++ src/NovCiklus.java
@@ -0,0 +1,20 @@
+
+public class NovCiklus {
+	
+		public static void main(String[] args) {
+			int x=2,y=6;
+			
+			if(x!=y) {
+				System.out.println("1");
+			}
+			if(x>y) {
+				System.out.println("2");
+			}
+			if(x%y == 0) {
+				System.out.println("3");
+			}
+			
+		}
+
+	}
+
