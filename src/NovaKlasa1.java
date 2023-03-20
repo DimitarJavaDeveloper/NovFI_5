39b584519dd42387621272a05221e75bdb94ab63
unknown <dimitarbelcovski29@gmail.com>
2023-03-20 11:44:38 +0100

    Message

---
+++ src/NovaKlasa1.java
@@ -0,0 +1,26 @@
+
+public class NovaKlasa1 {
+	public static void main(String[] args) {
+		boolean p,q;
+		
+		Scanner(System.in);
+		System.out.println("Input p:");
+		p = keyboard.nextBoolean();
+		System.out.println("Input q:");
+		q = keyboard.nextBoolean();
+		
+		if(p && q) {
+			q = false;
+		}else {
+			if(!q) {
+				System.out.println(p);
+			}
+			if(p == q) {
+				System.out.println(p || q);
+			}
+		}
+		
+		System.out.println(q);
+	}
+
+}
